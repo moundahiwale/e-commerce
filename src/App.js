8f1305fd988602a2d9341b94c9197d628eaf5e12
Moun <moun.dahiwale@hmhco.com>
2019-12-03 15:13:23 +0000

    Added homepage component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./components/homepage/homepage";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
